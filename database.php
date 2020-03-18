a8b0f5ae3a91f806087ba8a6cb207f5ee3155669
Abdullah Al Mahi <amahi.wp@gmail.com>
2020-03-19 00:55:41 +0600

    Update database.php

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 define("HOST","Localhost");
 define("DB_NAME","kufa");
 define("USER_NAME","root");
-define("PASSWORD","nishpa");
+define("PASSWORD","");
 
 $db = mysqli_connect(HOST,USER_NAME,PASSWORD,DB_NAME);
 
